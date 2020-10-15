24eaf0322542db39fe6d9162dc49a084d347df82
WalterJuarez <68527309+WalterJuarez@users.noreply.github.com>
2020-10-14 23:12:40 -0600

    creacion de la validacion

--- src/java/Model/Conexion.java
+++ src/java/Model/Conexion.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Model;
 
-/**
- *
- * @author 15109
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Conexion {
+    
+    private static String url = "jdbc:postgresql://localhost:5432/SistemaDeVentas";
+    private static String user = "postgres";
+    private static String password = "97135795182465";
+    
+    /**
+     * 
+     * @return
+     */
+    
+    public static Connection Conectar() throws ClassNotFoundException{
+        Connection con = null;
+        
+        try{
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(url, user, password);
+            java.sql.Statement st = con.createStatement();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        
+        return con;
+    }
     
 }
