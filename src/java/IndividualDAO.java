8b6f8f346b4b2cb651020bd676657ebac325cf85
WalterJuarez <68527309+WalterJuarez@users.noreply.github.com>
2020-10-28 08:43:25 -0600

    archivo faltante

---
+++ src/java/IndividualDAO.java
@@ -0,0 +1,22 @@
+import Model.Conexion;
+import Model.Individual;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IndividualDAO {
+    
+    /*public List<Individual> getDBIndividual(){
+        List<Individual> individual = new ArrayList();
+        try{
+            Statement statement = Conexion
+            
+        }catch(SQLException throwables){
+            throwables.printStackTrace();
+            
+        }
+    }*/
+}
